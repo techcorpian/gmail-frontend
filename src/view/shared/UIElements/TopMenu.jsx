1bf82ec2e5662524c0f7a7222b5a32e46a9a4cd0
mushthaq <techcorpian@gmail.com>
2024-10-01 09:09:23 +0530

    top menu dropdown bug resolved

--- src/view/shared/UIElements/TopMenu.jsx
+++ src/view/shared/UIElements/TopMenu.jsx
@@ -52,47 +52,49 @@ const TopMenu = ({ onSearch }) => {
         </div>
       </div>
 
-      <div className="flex items-center gap-4 relative inline-block text-left">
+      <div className="flex items-center gap-4">
         <div className="text-2xl"><IoMdHelpCircleOutline /></div>
         <div className="text-2xl"><IoSettingsOutline /></div>
         <div className="text-2xl"><CgMenuGridO /></div>
+        <div className="relative inline-block text-left">
 
-        {/* Button to trigger the dropdown */}
-        <button
-          onClick={toggleDropdown}
-          className="inline-flex justify-center items-center w-8 h-8 bg-blue-500 text-white rounded-full hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
-        >
-          <div className="text-lg font-medium">{initial}</div>
-        </button>
 
-        {/* Conditionally rendered dropdown */}
-        {isDropdownOpen && (
-          <div className="flex flex-col gap-6 p-4 z-20 justify-center items-center absolute right-0 mt-2 w-[400px] rounded-3xl shadow-lg bg-cyan-200 ring-1 ring-black ring-opacity-5 z-10">
-            <div className="text-sm">{currentUser.email}</div>
-            <div className="flex flex-col justify-center items-center gap-2">
-              <button
-                onClick={toggleDropdown}
-                className="w-20 h-20 bg-blue-500 text-white rounded-full hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
-              >
-                <div className="text-5xl font-medium pb-1">{initial}</div>
-              </button>
-              <div className="text-2xl">Hi, {currentUser.firstname.charAt(0).toUpperCase() + currentUser.firstname.slice(1)}!</div>
-              <button className="border border-gray-500 rounded-full text-sm p-2 px-6 text-blue-800 font-semibold">Manage Your Google Account</button>
-            </div>
-            <div className="flex flex-col p-1 px-6 bg-white rounded-3xl w-full">
-              <div className="flex items-center gap-3 border-b py-3">
+          {/* Button to trigger the dropdown */}
+          <button
+            onClick={toggleDropdown}
+            className="inline-flex justify-center items-center w-8 h-8 bg-blue-500 text-white rounded-full hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+          >
+            <div className="text-lg font-medium">{initial}</div>
+          </button>
+
+          {/* Conditionally rendered dropdown */}
+          {isDropdownOpen && (
+            <div className="flex flex-col gap-6 p-4 z-20 justify-center items-center absolute right-0 mt-2 w-[400px] rounded-3xl shadow-lg bg-cyan-200 ring-1 ring-black ring-opacity-5 z-10">
+              <div className="text-sm">{currentUser.email}</div>
+              <div className="flex flex-col justify-center items-center gap-2">
                 <button
                   onClick={toggleDropdown}
-                  className="w-10 h-10 bg-blue-500 text-center text-white rounded-full hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+                  className="w-20 h-20 bg-blue-500 text-white rounded-full hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
                 >
-                  <div className="text-lg font-medium">{initial}</div>
+                  <div className="text-5xl font-medium pb-1">{initial}</div>
                 </button>
-                <div>
-                  <div>{currentUser.firstname}</div>
-                  <div className="text-sm">{currentUser.email}</div>
-                </div>
+                <div className="text-2xl">Hi, {currentUser.firstname.charAt(0).toUpperCase() + currentUser.firstname.slice(1)}!</div>
+                <button className="border border-gray-500 rounded-full text-sm p-2 px-6 text-blue-800 font-semibold">Manage Your Google Account</button>
               </div>
-              {/* <div className="flex items-center gap-3 py-3">
+              <div className="flex flex-col p-1 px-6 bg-white rounded-3xl w-full">
+                <div className="flex items-center gap-3 border-b py-3">
+                  <button
+                    onClick={toggleDropdown}
+                    className="w-10 h-10 bg-blue-500 text-center text-white rounded-full hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+                  >
+                    <div className="text-lg font-medium">{initial}</div>
+                  </button>
+                  <div>
+                    <div>{currentUser.firstname}</div>
+                    <div className="text-sm">{currentUser.email}</div>
+                  </div>
+                </div>
+                {/* <div className="flex items-center gap-3 py-3">
                 <button
                   onClick={toggleDropdown}
                   className="w-10 h-10 bg-blue-500 text-center text-white rounded-full hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
@@ -104,16 +106,18 @@ const TopMenu = ({ onSearch }) => {
                   <div className="text-sm">{currentUser.email}</div>
                 </div>
               </div> */}
-              <div className="flex items-center gap-3 p-4 cursor-pointer" onClick={handleLogout}><LuLogOut className="text-xl" /> <span>Sign out from the account</span></div>
-            </div>
+                <div className="flex items-center gap-3 p-4 cursor-pointer" onClick={handleLogout}><LuLogOut className="text-xl" /> <span>Sign out from the account</span></div>
+              </div>
 
-            {/* <ul className="flex items-center gap-3 p-3 bg-white rounded-3xl w-full">
+              {/* <ul className="flex items-center gap-3 p-3 bg-white rounded-3xl w-full">
               <li className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 cursor-pointer">Option 1</li>
               <li className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 cursor-pointer">Option 2</li>
               <li className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 cursor-pointer">Option 3</li>
             </ul> */}
-          </div>
-        )}
+            </div>
+
+          )}
+        </div>
       </div>
     </div>
   )
